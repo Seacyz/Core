160fd2e2d056fd54f0f8f4f0dad29aa03b0f2414
geeklux <geeklux@sina.com>
2016-11-08 17:27:06 +0800

    add loadingView

---
+++ app/src/main/java/com/android/core/base/UIView.java
@@ -0,0 +1,9 @@
+package com.android.core.base;
+
+/**
+ * Created by bug on 2016/11/8.
+ */
+public interface UIView {
+    void showLoadingView();
+    void hideLoadingView();
+}
---
+++ app/src/main/java/com/android/core/http/LoadingInterceptor.java
@@ -0,0 +1,41 @@
+package com.android.core.http;
+
+import com.android.core.base.UIView;
+
+import java.io.IOException;
+
+import okhttp3.Connection;
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
+
+/**
+ * Created by bug on 2016/11/8.
+ */
+public class LoadingInterceptor implements Interceptor {
+    private UIView uiView;
+    public LoadingInterceptor(UIView uiView) {
+        this.uiView=uiView;
+    }
+
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        Request request = chain.request();
+        Response proceed = chain.proceed(request);
+        if(request!=null&&proceed==null){
+            showLoading();
+        }else if(proceed!=null){
+            hideLoading();
+        }
+        return proceed;
+    }
+
+    private void hideLoading() {
+        uiView.showLoadingView();
+    }
+
+    private void showLoading() {
+        uiView.hideLoadingView();
+    }
+}
